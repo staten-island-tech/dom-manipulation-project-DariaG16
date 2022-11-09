b753cf63a0e90b5c5c3ccc399fb69aeb07861ecb
DariaG16 <dariagolitsyna16@gmail.com>
2022-11-08 19:48:17 -0500

    a

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const DOMSelectors = {
   skilltxt: document.querySelector(`#skilltxt`),
   clothestxt: document.querySelector(`#clothestxt`),
   expressiontxt: document.querySelector(`#expressiontxt`),
+  output: document.querySelector(`#output`),
+  images: document.querySelector(`images`),
 };
 
 DOMSelectors.button.addEventListener("click", function () {
@@ -17,17 +19,23 @@ DOMSelectors.button.addEventListener("click", function () {
   let skill = DOMSelectors.skilltxt.value;
   let clothes = DOMSelectors.clothestxt.value;
   let expression = DOMSelectors.expressiontxt.value; // we get the value of whatever the user typed in the text box named "expression"
-  DOMSelectors.doughType.insertAdjacentHTML(
-    `beforebegin`,
+
+  DOMSelectors.output.insertAdjacentHTML(
+    `beforeend`,
     `<p>${doughType}</p> <p>${rarity}</p>  <p>${skill}</p>  <p>${clothes}</p>  <p>${expression}</p>`
   ); //whatever is put in the doughtype box comes out under it
-  DOMSelectors.doughTypetxt.value = "";
+  /*   DOMSelectors.doughTypetxt.value = "";
   DOMSelectors.raritytxt.value = "";
   DOMSelectors.skill.value = "";
   DOMSelectors.clothes.value = "";
-  DOMSelectors.expression.value = "";
+  DOMSelectors.expression.value = ""; */
   //resetting the text box to clear every time you click the button
 });
+
+function showImage(images) {
+  images.style.visibility = "visible";
+}
+DOMSelectors.button.addEventListener("click", showImage(images));
 
 /* const cat = "meow";
 DOMSelectors.box.insertAdjacentHTML("afterend", `<h1>We are an ${cat} </h1>`); //2 arguments: where were putting it and what were putting in it. */
@@ -40,14 +48,4 @@ function makeCookie() {
   //querySelectorAll returns node list of buttons
   //foreach button add an event listener.
   //function (e) for event.target
-}
-
-function cryingmanytears() {
-  /* Get image and change value
-  of src attribute */
-  let image = document.getElementById("image");
-
-  image.src = "Images/ChocoDough.png";
-
-  document.getElementById("btn").style.display = "none";
 }

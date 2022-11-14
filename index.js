15c2333d335fefa779df02a3a32d6ecfe0e24eb0
DariaG16 <dariagolitsyna16@gmail.com>
2022-11-14 14:01:45 -0500

    sdfgsfgfsd

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const DOMSelectors = {
   skilltxt: document.querySelector(`#skilltxt`),
   clothestxt: document.querySelector(`#clothestxt`),
   expressiontxt: document.querySelector(`#expressiontxt`),
-  output: document.querySelector(`#output`),
   images: document.querySelector(`images`),
 
   choco: document.querySelector(`#choco`),
@@ -38,35 +37,68 @@ DOMSelectors.button.addEventListener("click", function () {
   let skill = DOMSelectors.skilltxt.value;
   // we get the value of whatever the user typed in the text box named "expression"
 
-  DOMSelectors.output.insertAdjacentHTML(
-    `beforeend`,
-    `<p>Rarity: ${rarity}</p>  <p>Skill Type: ${skill}</p> `
+  DOMSelectors.insertAdjacentHtml(
+    `
+    <h2>Cookie:</h2>
+    <p>Rarity: ${rarity}</p>  <p>Skill Type: ${skill}</p> <button id="delete"> Delete Cookie</button>
+    
+    `
   ); //whatever is put in the doughtype box comes out under it
   DOMSelectors.raritytxt.value = "";
   DOMSelectors.skilltxt.value = "";
   //resetting the text box to clear every time you click the button
 });
-
+//buttons
 DOMSelectors.darkBtn.addEventListener("click", function () {
-  let choco = DOMSelectors.choco.value;
+  let choco = DOMSelectors.choco;
+  choco.style.visibility = "visible";
 });
-/* function showImage(images) {
-  images.style.visibility = "visible";
-}
-DOMSelectors.button.addEventListener("click", showImage(images)); */
+DOMSelectors.vanillaBtn.addEventListener("click", function () {
+  let vanilla = DOMSelectors.vanilla;
+  vanilla.style.visibility = "visible";
+  vanilla.style.backgroundcolor = "red";
+});
+DOMSelectors.gingerBtn.addEventListener("click", function () {
+  let ginger = DOMSelectors.ginger;
+  ginger.style.visibility = "visible";
+});
+DOMSelectors.suitBtn.addEventListener("click", function () {
+  let suit = DOMSelectors.suit;
+  suit.style.visibility = "visible";
+});
+DOMSelectors.dressBtn.addEventListener("click", function () {
+  let dress = DOMSelectors.dress;
+  dress.style.visibility = "visible";
+});
+DOMSelectors.capeBtn.addEventListener("click", function () {
+  let cape = DOMSelectors.cape;
+  cape.style.visibility = "visible";
+});
+DOMSelectors.smilingBtn.addEventListener("click", function () {
+  let smiling = DOMSelectors.smiling;
+  smiling.style.visibility = "visible";
+});
+DOMSelectors.frowningBtn.addEventListener("click", function () {
+  let frowning = DOMSelectors.frowning;
+  frowning.style.visibility = "visible";
+});
+DOMSelectors.angryBtn.addEventListener("click", function () {
+  let angry = DOMSelectors.angry;
+  angry.style.visibility = "visible";
+});
 
 /* const cat = "meow";
 DOMSelectors.box.insertAdjacentHTML("afterend", `<h1>We are an ${cat} </h1>`); //2 arguments: where were putting it and what were putting in it. */
 
 //REMOVING THE COOKIE
 
-function makeCookie() {
+/* function makeCookie() {
   //make the card
   //get delete button
   //querySelectorAll returns node list of buttons
   //foreach button add an event listener.
   //function (e) for event.target
-}
+} */
 
 /* document.getElementByyId("form").addEventListener(
   "submit",
